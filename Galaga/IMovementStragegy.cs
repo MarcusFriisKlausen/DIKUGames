8c69504e9d81a00b3dfd187b1a03ed10b086a82f
MarcusFriisKlausen <qgt728@alumni.ku.dk>
2023-03-22 20:22:53 +0100

    qew

--- Galaga/IMovementStragegy.cs
+++
@@ -1,8 +0,0 @@
-using DIKUArcade.Entities;
-
-namespace Galaga.MovementStrategy {
-    public interface IMovementStrategy {
-        void MoveEnemy (Enemy enemy);
-        void MoveEnemies (EntityContainer<Enemy> enemies);
-        }
-} 
