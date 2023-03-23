c3719499cb2beccc9f96926dd379cc3044cad934
CarolineHelene <126587564+CarolineHelene@users.noreply.github.com>
2023-03-23 10:41:19 +0100

    Rettet referencer til test

--- GalagaTests/TestEnemy.cs
+++ GalagaTests/TestEnemy.cs
@@ -1,15 +1,30 @@
-namespace GalagaTest;
+/* using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using NUnit.Framework;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using Galaga;
 
-public class TestEnemy
-{
-    [SetUp]
-    public void Setup()
-    {
+namespace GalagaTest {
+    [TestFixture]
+    public class TestEnemy {
+        private List<Image> enemyStride;
+        private List<Image> alternativeEnemyStride;
+        private Enemy enemy;
+        
+        [SetUp]
+        public void InitializeEnemy() {
+            enemyStride = new List<Image>();
+            alternativeEnemyStride = new List<Image>();
+            Isquadron.CreateEnemies(enemyStride, alternativeEnemyStride);
+            enemy = new Enemy(new DynamicShape(0.0f, 0.0f, 1.0f, 1.0f), 
+                              enemyStride, alternativeEnemyStride);
+        }
+        
+        [Test]
+        public void TestIsEnraged() {
+            enemy.Enrage();
+            Assert.That(enemy.ImageStride, Is.EqualTo(enemy.AlternativeImageStride));
+        }
     }
-
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
-}
+} */
