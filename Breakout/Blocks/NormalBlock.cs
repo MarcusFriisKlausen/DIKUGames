215b6b0998f9b8cf6e24e398bf5dfdad37d91609
CarolineHelene <126587564+CarolineHelene@users.noreply.github.com>
2023-06-07 11:50:57 +0200

    slettet iblock
    Hvorfor bliver den ved med at være her???

--- Breakout/Blocks/NormalBlock.cs
+++ Breakout/Blocks/NormalBlock.cs
@@ -15,16 +15,4 @@ public class NormalBlock : Block {
             this.Image = image;
             this.blockImage = image;
     }
-
-    public InvisibleBlock ToInvisible() {
-        return new InvisibleBlock(Shape.AsDynamicShape(), Image, brokenImage);
-    }
-
-    public UnbreakableBlock ToUnbreakable() {
-        return new UnbreakableBlock(Shape.AsDynamicShape(), Image, brokenImage);
-    }
-
-    public PowerUpBlock ToPowerUp() {
-        return new PowerUpBlock(Shape.AsDynamicShape(), Image, brokenImage);
-    }
 }
